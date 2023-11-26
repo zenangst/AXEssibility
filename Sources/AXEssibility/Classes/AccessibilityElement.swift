8e1da7cd1e8062672f9b9dcc4344f9876019d4dc
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-26 18:00:59 +0100

    Move position, size, frame and performing actions to protocol extension

--- Sources/AXEssibility/Classes/AccessibilityElement.swift
+++ Sources/AXEssibility/Classes/AccessibilityElement.swift
@@ -39,6 +39,33 @@ extension AccessibilityElement {
     reference.findAttribute(attribute, of: role)
   }
 
+  public var position: CGPoint? {
+    get { return try? value(.position) }
+    set { guard let value = AXValue.from(value: newValue, type: .cgPoint) else { return }
+      AXUIElementSetAttributeValue(reference, kAXPositionAttribute as CFString, value)
+    }
+  }
+
+  public var size: CGSize? {
+    get { return try? value(.size) }
+    set { guard let value = AXValue.from(value: newValue, type: .cgSize) else { return }
+      AXUIElementSetAttributeValue(reference, kAXSizeAttribute as CFString, value)
+    }
+  }
+
+  public var frame: CGRect? {
+    get {
+      guard let origin = position, let size = size else { return nil }
+      return CGRect(origin: origin, size: size)
+    }
+    set {
+      guard let newValue else { return }
+      let newFrame = CGRect(origin: newValue.origin, size: newValue.size)
+      position = newFrame.origin
+      size = newFrame.size
+    }
+  }
+
   // MARK: Internal methods
 
   internal func debugValue(_ value: Any?) -> String {
@@ -57,5 +84,13 @@ extension AccessibilityElement {
 
   private func anyValue(_ attribute: String) throws -> Any {
     try reference.rawValue(for: attribute)
+  }
+
+  // MARK: Actions
+
+  @discardableResult
+  public func performAction(_ action: AnyAccessibilityElement.Action) -> Self {
+    AXUIElementPerformAction(reference, action.rawValue as CFString)
+    return self
   }
 }
--- Sources/AXEssibility/Classes/AnyAccessibilityElement.swift
+++ Sources/AXEssibility/Classes/AnyAccessibilityElement.swift
@@ -1,6 +1,26 @@
 import Cocoa
 
 public final class AnyAccessibilityElement: AccessibilityElement {
+  public enum Action {
+    case raise
+    case showMenu
+    case pick
+    case press
+
+    var rawValue: String {
+      switch self {
+      case .raise:
+        return kAXRaiseAction
+      case .showMenu:
+        return kAXShowMenuAction
+      case .pick:
+        return kAXPickAction
+      case .press:
+        return kAXPressAction
+      }
+    }
+  }
+
   public private(set) var reference: AXUIElement
 
   public init(_ reference: AXUIElement) {
